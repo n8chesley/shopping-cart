3869846e7efce6a0b66d9552cc2102d4fa1a4246
nate chesley <nate@almanac.local>
2017-02-23 10:45:59 +0200

    some changes

--- public/server.js
+++
@@ -1,7 +0,0 @@
-var express = require('express');
-var app = express();
-app.listen(8000);
-app.use(express.static('public'));
-app.get('/', function(request, response){
-  response.send("hello");
-});
